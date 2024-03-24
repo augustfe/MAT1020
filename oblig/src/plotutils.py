d38e92f0bab062d2eecdc1e7844ba8d10f66999b
August Femtehjell <august.femtehjell@gmail.com>
2024-03-24 17:54:34 +0100

    Initial file dump

---
+++ oblig/src/plotutils.py
@@ -0,0 +1,175 @@
+import seaborn as sns
+import numpy as np
+import matplotlib as mpl
+from pathlib import Path
+from pandas import DataFrame
+from matplotlib import colormaps, pyplot as plt, cm
+
+
+# Set up for LaTeX rendering
+mpl.rcParams["mathtext.fontset"] = "stix"
+mpl.rcParams["font.family"] = "STIXGeneral"
+mpl.rcParams["figure.titlesize"] = 15
+
+
+class TimeSeriesPlotter:
+    month_by_season = np.array(
+        [
+            None,
+            "Winter",
+            "Winter",
+            "Spring",
+            "Spring",
+            "Spring",
+            "Summer",
+            "Summer",
+            "Summer",
+            "Autumn",
+            "Autumn",
+            "Autumn",
+            "Winter",
+        ]
+    )
+
+    def __init__(
+        self,
+        X: DataFrame,
+        period: str,
+        cmap: str = "coolwarm",
+        datalabel: str = None,
+        save_dir: Path = None,
+    ) -> None:
+        """Plotting utilities for time series data
+
+        Args:
+            X (DataFrame): Data to plot
+            period (str): Period of the data
+            cmap (str, optional): Color map for heatmaps. Defaults to "coolwarm".
+            datalabel (str, optional): Label for data. Defaults to None.
+        """
+        self.X = X
+        self.period = period
+        self.cmap = cmap
+        self.datalabel = datalabel
+        if save_dir:
+            save_dir.mkdir(parents=True, exist_ok=True)
+        self.save = save_dir
+
+    def plot_heatmap(
+        self,
+        X: DataFrame,
+        title: str,
+        mask: np.ndarray = None,
+    ) -> None:
+        """Plot a heatmap of the data
+
+        Args:
+            X (DataFrame): Data to plot
+            title (str): Title of the plot
+            mask (np.ndarray, optional): Whether to mask the plot or not. Defaults to None.
+        """
+        ax = sns.heatmap(
+            X,
+            cmap=self.cmap,
+            annot=True,
+            square=True,
+            linewidth=0.5,
+            mask=mask,
+        )
+        ax.set_title(title)
+        if self.save:
+            plt.savefig(self.save / f"{title}.pdf", bbox_inches="tight")
+
+        ax.plot()
+
+    def plot_df(self, X: DataFrame, title: str) -> None:
+        """Plot a DataFrame
+
+        Args:
+            X (DataFrame): Data to plot
+            title (str): Title of the plot
+        """
+        ax = X.plot(title=title, ylabel=self.datalabel)
+        if self.save:
+            plt.savefig(self.save / f"{title}.pdf", bbox_inches="tight")
+        ax.plot()
+
+    def plot_raw(self) -> None:
+        """Plot the raw data"""
+        title = f"{self.datalabel} {self.period}"
+        self.plot_df(self.X, title)
+
+    def cumsum_rolling(self, year: int = None, window: int = 30) -> None:
+        """Plot the rolling mean and cumulative sum of the data
+
+        Args:
+            year (int, optional): Year of the data. Default to None.
+            window (int, optional): Window size for rolling mean. Defaults to 30.
+        """
+        if year:
+            X = self.X[self.X.index.year == year]
+            base_title = f"{self.datalabel} {year}"
+        else:
+            X = self.X
+            base_title = f"{self.datalabel} {self.period}"
+        rolling_title = f"{base_title} - Rolling {window} day"
+        self.plot_df(X.rolling(window=window).mean(), rolling_title)
+
+        cumsum_title = f"{base_title} - Cumulative Sum"
+        self.plot_df(X.cumsum(), cumsum_title)
+
+    def mean_by_season(self, year: int = None) -> None:
+        """Plot the mean of the data by season
+
+        Args:
+            year (int, optional): Year of the data. Default to None.
+        """
+        if year:
+            X = self.X[self.X.index.year == year]
+            title = f"{year}"
+        else:
+            X = self.X
+            title = f"{self.period}"
+
+        groups = self.month_by_season[X.index.month]
+        X_season = X.copy()
+        X_season.insert(0, "Season", groups)
+
+        season_title = f"{self.datalabel} {title} - Mean by Season"
+
+        self.plot_heatmap(X_season.groupby(["Season"]).mean(), season_title)
+
+    def cov(self, year: int = None) -> None:
+        """Plot the covariance of the data
+
+        Args:
+            year (int, optional): Year of the data. Default to None.
+        """
+        if year:
+            cov = self.X[self.X.index.year == year].cov()
+            cov_title = f"{self.datalabel} {year} - Covariance"
+        else:
+            cov = self.X.cov()
+            cov_title = f"{self.datalabel} {self.period} - Covariance"
+
+        mask = np.triu(np.ones_like(cov, dtype=bool), k=1)
+        self.plot_heatmap(cov, cov_title, mask=mask)
+
+    def corr(
+        self,
+        year: int = None,
+    ) -> None:
+        """Plot the correlation of the data
+
+        Args:
+            year (int, optional): Year of the data. Default to None.
+        """
+        if year:
+            corr = self.X[self.X.index.year == year].corr()
+            corr_title = f"{self.datalabel} {year} - Correlation"
+        else:
+            corr = self.X.corr()
+            corr_title = f"{self.datalabel} {self.period} - Correlation"
+        mask = np.triu(np.ones_like(corr, dtype=bool))
+
+        self.plot_heatmap(corr, corr_title, mask=mask)
